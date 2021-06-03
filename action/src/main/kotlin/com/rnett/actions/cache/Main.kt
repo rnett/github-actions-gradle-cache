11519894c6a4e5e69514d0758e50b05b4645d3af
Ryan Nett <JNett96@gmail.com>
2021-06-02 18:42:50 -0700

    Fix path

--- action/src/main/kotlin/com/rnett/actions/cache/Main.kt
+++ action/src/main/kotlin/com/rnett/actions/cache/Main.kt
@@ -2,8 +2,9 @@ package com.rnett.actions.cache
 
 import com.rnett.action.Path
 import com.rnett.action.core.env
+import com.rnett.action.core.runOrFail
 
-fun main() {
+fun main() = runOrFail{
     println("Test")
     val baseUrl = (
             env["ACTIONS_CACHE_URL"] ?: env["ACTIONS_RUNTIME_URL"] ?: error("Cache Service Url not found"))
