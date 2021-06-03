d69c632370e29aafc8fd00901d9b74bad0a88671
Ryan Nett <JNett96@gmail.com>
2021-06-02 19:39:14 -0700

    Update action

--- action/src/main/kotlin/com/rnett/actions/cache/Main.kt
+++ action/src/main/kotlin/com/rnett/actions/cache/Main.kt
@@ -1,6 +1,5 @@
 package com.rnett.actions.cache
 
-import com.rnett.action.Path
 import com.rnett.action.core.env
 import com.rnett.action.core.maskSecret
 import com.rnett.action.core.runOrFail
@@ -11,10 +10,10 @@ fun main() = runOrFail{
             env["ACTIONS_CACHE_URL"] ?: env["ACTIONS_RUNTIME_URL"] ?: error("Cache Service Url not found"))
         .replace("pipelines", "artifactcache") + "_apis/artifactcache/"
 
-    println("BaseURL: $baseUrl")
-//    maskSecret(baseUrl)
+    maskSecret(baseUrl)
     env.export[EnviromentVariables.baseUrl] = baseUrl
 
     val runtimeToken = env["ACTIONS_RUNTIME_TOKEN"] ?: error("Could not get cache access token")
+    maskSecret(runtimeToken)
     env.export[EnviromentVariables.token] = runtimeToken
 }
